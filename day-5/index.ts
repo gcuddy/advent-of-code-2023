24c135d424f94d203f3da789bc57fe2088d00d41
Gus <gus.cuddy@gmail.com>
2023-12-05 12:23:55 -0500

    works

--- day-5/index.ts
+++ day-5/index.ts
@@ -1,24 +1,25 @@
 const num_regex = /(\d+)/g;
 const map_regex = /\w.*?map\:/g;
 
+type MapLine = {
+  source: number;
+  dest: number;
+  range: number;
+};
+
 // q: is there a way to map the maps semantically? instead of just following procedurally i.e. going map a -> b -> c ... f (and then proclaiming a -> f)
 export function part1(input: string): number {
   const locations: number[] = [];
   const lines = input.split("\n");
   const seeds = parse_numbers(lines[0]);
   const maps = lines.slice(2).join("\n").split(map_regex).slice(1);
-  console.log({ seeds, maps });
   for (const seed of seeds) {
     let curr = seed;
     // is there a way to avoid this n^2?
-    console.log({ curr });
     for (const map of maps) {
-      console.log(`first map`, map);
       const m = parse_map(map.split("\n"));
-      console.log({ m });
-      curr = lookup(m, curr);
+      curr = lookup_line(m, curr);
     }
-    // console.log(seed);
     locations.push(curr);
   }
 
@@ -40,20 +41,48 @@ export function parse_numbers(line: string): number[] {
 
 const lookup = (map: Map<number, number>, n: number) => map.get(n) ?? n;
 
-export function parse_map(lines: string[]) {
-  // source -> dest
-  const map = new Map<number, number>();
-  // dest, source, range
-  console.log("parsing map", lines);
-  lines.forEach((line) => {
-    // protect against empty lines
-    if (!line.trim()) return;
-    const [d, s, r] = parse_numbers(line);
-    console.log({ d, s, r });
-    for (let i = 0; i < r; i++) {
-      map.set(s + i, d + i);
+const lookup_line = (lines: MapLine[], n: number) => {
+  // if n is within source mapping, then get destination mapping. else return n
+  for (const line of lines) {
+    const { dest, range, source } = line;
+    const offset = dest - source;
+    if (n >= source && n <= source + range - 1) {
+      return n + offset;
     }
-  });
+  }
+  return n;
+};
+
+export function parse_map(lines: string[]): Array<MapLine> {
+  // source -> dest
+  // dest, source, range
+  //   console.log("parsing map", lines);
+  return lines
+    .map((line) => {
+      // protect against empty lines
+      if (!line.trim()) return;
+      const [dest, source, range] = parse_numbers(line);
+      // console.log({ d, s, r });
+      return {
+        dest,
+        source,
+        range,
+      };
+      // for (let i = 0; i < r; i++) {
+      //   map.set(s + i, d + i);
+      // }
+    })
+    .filter(Boolean) as Array<MapLine>;
+  // const map = new Map<number, number>();
+  //   lines.forEach((line) => {
+  //     // protect against empty lines
+  //     if (!line.trim()) return;
+  //     const [d, s, r] = parse_numbers(line);
+  //     console.log({ d, s, r });
+  //     for (let i = 0; i < r; i++) {
+  //       map.set(s + i, d + i);
+  //     }
+  //   });
   return map;
 }
 
