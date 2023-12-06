88b5b3cf199a3e5b7e8505f334edec06236a0545
Gus <gus.cuddy@gmail.com>
2023-12-05 23:39:35 -0500

    trying to get ranges working

--- day-5/day-5.test.ts
+++ day-5/day-5.test.ts
@@ -1,5 +1,12 @@
 import { expect, test } from "bun:test";
-import { parse_numbers, part1, part2 } from ".";
+import {
+  find_smallest_output,
+  parse_map,
+  parse_numbers,
+  part1,
+  part2,
+  process_range,
+} from ".";
 
 test("part 1", async () => {
   const f = Bun.file("./example.txt");
@@ -17,5 +24,187 @@ test("get seeds", () => {
   expect(parse_numbers(`seeds: 79 14 55 13`)).toEqual([79, 14, 55, 13]);
 });
 
+// test("smallest output", () => {
+//   const lines = `50 98 2
+//     52 50 48`;
+//   const l = parse_map(lines.split("\n"));
+//   const range = [79, 92] as const;
+//   expect(find_smallest_output(l, range)).toBe(82);
+// });
 
+test("range processing: seed-to-soil", () => {
+  expect(
+    process_range(
+      [[79, 92]],
+      [
+        {
+          start: 50,
+          end: 97,
+          offset: 2,
+        },
+        {
+          start: 98,
+          end: 99,
+          offset: -48,
+        },
+      ]
+    )
+  ).toEqual([[81, 94]]);
+});
 
+test("range processing: soil to fertilizer", () => {
+  expect(
+    process_range(
+      [[81, 94]],
+      [
+        {
+          start: 15,
+          end: 51,
+          offset: -15,
+        },
+        {
+          start: 52,
+          end: 53,
+          offset: -15,
+        },
+        {
+          start: 0,
+          end: 14,
+          offset: 39,
+        },
+      ]
+    )
+  ).toEqual([[81, 94]]);
+});
+
+test("range processing: fertilizer-to-water", () => {
+  expect(
+    process_range(
+      [[81, 94]],
+      [
+        {
+          start: 53,
+          end: 60,
+          offset: -4,
+        },
+        {
+          start: 11,
+          end: 52,
+          offset: -11,
+        },
+        {
+          start: 0,
+          end: 6,
+          offset: 42,
+        },
+        {
+          start: 7,
+          end: 10,
+          offset: 50,
+        },
+      ]
+    )
+  ).toEqual([[81, 94]]);
+});
+
+test("range processing: water-to-light", () => {
+  expect(
+    process_range(
+      [[81, 94]],
+      [
+        {
+          start: 18,
+          end: 24,
+          offset: 70,
+        },
+        {
+          start: 25,
+          end: 94,
+          offset: -7,
+        },
+      ]
+    )
+  ).toEqual([[74, 87]]);
+});
+
+test("range processing: light-to-temperature", () => {
+  expect(
+    process_range(
+      [[74, 87]],
+      [
+        {
+          start: 77,
+          end: 99,
+          offset: -32,
+        },
+        {
+          start: 45,
+          end: 63,
+          offset: 36,
+        },
+        {
+          start: 64,
+          end: 76,
+          offset: 4,
+        },
+      ]
+    )
+  ).toEqual([
+    [45, 55],
+    [78, 80],
+  ]);
+});
+
+test("range processing: temperature-to-humidity", () => {
+  expect(
+    process_range(
+      [
+        [45, 55],
+        [78, 80],
+      ],
+      [
+        {
+          start: 69,
+          end: 69,
+          offset: -69,
+        },
+        {
+          start: 0,
+          end: 68,
+          offset: 1,
+        },
+      ]
+    )
+  ).toEqual([
+    [46, 56],
+    [78, 80],
+  ]);
+});
+
+test("range processing: humidity-to-location", () => {
+  expect(
+    process_range(
+      [
+        [46, 56],
+        [78, 80],
+      ],
+      [
+        {
+          start: 56,
+          end: 92,
+          offset: 4,
+        },
+        {
+          start: 93,
+          end: 96,
+          offset: -37,
+        },
+      ]
+    )
+  ).toEqual([
+    // [56, 56],
+    [60, 60],
+    [46, 55],
+    [82, 84],
+  ]);
+});
--- day-5/index.ts
+++ day-5/index.ts
@@ -26,11 +26,12 @@ export function part1(input: string): number {
   return Math.min(...locations);
 }
 
+// need some way of calculating a -> f
+
 export function part2(input: string): number {
   const locations: number[] = [];
   const lines = input.split("\n");
   const seeds = parse_seed_range(lines[0]);
-  console.log({ seeds });
   const maps = lines.slice(2).join("\n").split(map_regex).slice(1);
   //   TODO: there's probably a way to cache/be smarter about seeds so we don't reprocess everything
   for (const seed of seeds) {
@@ -47,7 +48,7 @@ export function part2(input: string): number {
   return Math.min(...locations);
 }
 
-// move to utils
+// move to utils - also, this seems useful but won't actually be useful for the real inputs (which have millions of numbers - this will slow to a stop)
 export function range(start: number, end: number) {
   const output: number[] = [];
   for (let i = start; i < end; i++) {
@@ -94,6 +95,93 @@ const lookup_line = (lines: MapLine[], n: number) => {
   }
   return n;
 };
+
+// use this for rules
+type Range = {
+  start: number;
+  end: number;
+  offset: number;
+};
+
+// use this to track seed range over time
+type SRange = [number, number];
+type SeedRanges = SRange[];
+
+export function process_range(
+  existing_ranges: SeedRanges,
+  ranges: Range[]
+): SeedRanges {
+  const new_ranges: SeedRanges = [];
+  for (let er of existing_ranges) {
+    let has_match = false;
+    for (const range of ranges) {
+      if (er[0] >= range.start && range.end >= er[0] && range.end < er[1]) {
+        // then we have a split...
+        console.log("SPLIT!!!", er, range)
+        const n = [er[0] + range.offset, range.end + range.offset] as SRange;
+        er[0] = range.end + 1;
+        new_ranges.push(n)
+        has_match = true;
+        //   console.log(`now: `, er, range)
+      } else if (er[0] >= range.start && er[1] <= range.end) {
+        // then we're in the range
+        new_ranges.push([er[0] + range.offset, er[1] + range.offset]);
+        has_match = true;
+      } else if (er[1] <= range.end && range.start <= er[1]) {
+        console.log("END SPLIT!!!", er, range)
+        const n: SRange = [range.start + range.offset, er[1] + range.offset];
+        // new_ranges.push([])
+        er[1] = range.start - 1;
+        new_ranges.push(n);
+        // has_match = true;
+      }
+    }
+    if (!has_match) {
+      new_ranges.push(er);
+    }
+  }
+  //   for (const range of ranges) {
+  //     for (const er of existing_ranges) {
+  //       if (er[0] >= range.start && er[1] <= range.end) {
+  //         // then we're in the range
+  //         new_ranges.push([er[0] + range.offset, er[1] + range.offset]);
+  //       } else {
+  //         new_ranges.push(er);
+  //       }
+  //     }
+  //   }
+
+  console.log({new_ranges})
+  return new_ranges;
+}
+
+/**
+ * Given a range, this should find the smallest output somehow
+ */
+export function find_smallest_output(
+  lines: MapLine[],
+  n_range: Readonly<[number, number]>
+) {
+  // each line goes: [source, source + range] -> [dest, dest + range]
+  // given 79 - 92, we should check if the destination is higher or lower than the source (offset).
+  // if higher, then we want to avoid it. if lower, we want to seek it out.
+  //
+  let curr_smallest: [number, number] = [-1, -1];
+  for (const line of lines) {
+    const { dest, range, source } = line;
+    const offset = dest - source;
+    if (offset > 0) {
+      // destination -s higher
+      if (n_range[0] < source) {
+      }
+    } else {
+    }
+    // if (n >= source && n <= source + range - 1) {
+    //   return n + offset;
+    // }
+  }
+  return n;
+}
 
 // find smallest
 
